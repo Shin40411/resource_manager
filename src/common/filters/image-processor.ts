179a3e17b545cf39061b93cf90f0d8bfb9d7ed1b
Dang Tho <dtho961@gmail.com>
2025-03-25 00:37:09 +0700

    update details and upload unlink

--- src/common/filters/image-processor.ts
+++ src/common/filters/image-processor.ts
@@ -1,14 +1,26 @@
-import * as sharp from 'sharp';
 import * as fs from 'fs';
+import * as path from 'path';
+import * as sharp from 'sharp';
 
 export async function compressImage(filePath: string) {
-  const outputPath = filePath.replace(/(\.[\w\d_-]+)$/i, '_compressed$1');
+    const outputPath = filePath.replace(/(\.[\w\d_-]+)$/i, '_compressed$1');
 
-  await sharp(filePath)
-    .resize({ width: 1024 }) // Giảm kích thước ảnh
-    .jpeg({ quality: 70 })   // Nén ảnh
-    .toFile(outputPath);
+    await sharp(filePath)
+        .resize({ width: 1024 })
+        .jpeg({ quality: 70 })
+        .toFile(outputPath);
 
-  fs.unlinkSync(filePath); // Xóa ảnh gốc
-  return outputPath;
+    try {
+        await fs.promises.unlink(filePath); 
+    } catch (err) {
+        const imagesDir = path.dirname(filePath);
+        const tempDir = path.join(imagesDir, 'temp');
+
+        await fs.promises.mkdir(tempDir, { recursive: true });
+
+        const tempFilePath = path.join(tempDir, path.basename(filePath));
+        await fs.promises.rename(filePath, tempFilePath);
+    }
+
+    return outputPath;
 }
